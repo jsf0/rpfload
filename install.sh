4a5ae233b61ea5767ba51753f40abe25a16a8b83
Scott Bennett <scottb@fastmail.com>
2021-06-29 18:08:02 +0000

    Install man page to section 8

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 install -m 755 -d /usr/local/bin
 install -m 755 rpfload.pl /usr/local/bin/rpfload
-install -m 755 -d /usr/local/man/man1
-install -m 644 rpfload.1 /usr/local/man/man1
+install -m 755 -d /usr/local/man/man8
+install -m 644 rpfload.1 /usr/local/man/man8
