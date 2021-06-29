eded59046ec424b7e0c3ec9b72becf793b4425b8
Scott Bennett <scottb@fastmail.com>
2021-06-29 22:29:50 +0000

    sh script replaced with makefile

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-install -m 755 -d /usr/local/sbin
-install -m 755 rpfload.pl /usr/local/sbin/rpfload
-install -m 755 -d /usr/local/man/man8
-install -m 644 rpfload.8 /usr/local/man/man8
